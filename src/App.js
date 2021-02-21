cde0a4adba1a625fc543740e82f179286dc0a4b9
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-21 10:44:51 +0530

    Finished heder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "antd/dist/antd.css";
-import { Button } from "antd";
+import { Layout } from "antd";
+import AppHeader from "./components/common/header";
+import "./App.css";
+const { Header } = Layout;
 function App() {
   return (
     <div className="App">
-      <Button type="primary">Primary Button</Button>
+      <Layout className="mainLayout">
+        <Header>
+          <AppHeader />
+        </Header>
+      </Layout>
+      ,
     </div>
   );
 }
